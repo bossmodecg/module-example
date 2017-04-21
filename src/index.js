84c4ef8abdb509350829f16619d975a1efb83267
Ed Ropple <ed@edropple.com>
2017-04-20 23:29:47 -0400

    v0.2.0 - updated to new module API

--- src/index.js
+++ src/index.js
@@ -1,40 +1,44 @@
-import _ from 'lodash';
-
-import BossmodeCG from '@bossmodecg/core';
+import Module from '@bossmodecg/module';
 
 const DEFAULT_CONFIG =
   {
     countInterval: 1000
   };
 
-export default class ExampleModule extends BossmodeCG.BModule {
-  constructor(config) {
-    super("example", _.merge({}, DEFAULT_CONFIG, config));
-  }
+export default class ExampleModule extends Module {
+  constructor(config = {}) {
+    super("example", config);
 
-  async _doRegister(server, http) {
-    this.server.on('beforeRun', () => {
-      this.logger.debug("beforeRun hit.");
+    this.on("internal.registerServer", (server) => {
+      this.logger.debug("Registering server callbacks.");
+
+      server.on('internal.beforeRun', () => {
+        this.logger.info(`Setting up example counter every ${this.config.countInterval}ms.`);
+
+        setInterval(() => {
+          const newCount = (this._state.count || 0) + 1;
+          this.setState({ count: newCount });
+          this.logger.trace(`Updating count to ${newCount}.`);
+
+          this.emit('count', { count: newCount });
+        }, this.config.countInterval);
+      });
+    });
+
+    this.on("internal.registerHttp", (http) => {
+      http.post("/reset", () => {
+        this.logger.info("Resetting via synchronous command.");
+        this.setState({ count: 0 });
+      });
     });
 
     this.on('reset', () => {
       this.logger.info("Restting via socket.io event.");
       this.setState({ count: 0 });
     });
-
-    http.post("/reset", () => {
-      this.logger.info("Resetting via synchronous command.");
-      this.setState({ count: 0 });
-    });
-
-
-    this.logger.info(`Setting up example counter every ${this.config.countInterval}ms.`);
-    setInterval(() => {
-      const newCount = (this._state.count || 0) + 1;
-      this.setState({ count: newCount });
-      this.logger.trace(`Updating count to ${newCount}.`);
-
-      this.emit('count', { count: newCount });
-    }, this.config.countInterval);
   }
+
+  /* eslint-disable class-methods-use-this */
+  get defaultConfig() { return DEFAULT_CONFIG; }
+  /* eslint-enable class-methods-use-this */
 }
